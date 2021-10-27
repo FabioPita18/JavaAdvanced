9d7384c055e7226baead16ecc1c46f9562772ec0
FabioPita18 <fabiopita267@gmail.com>
2021-10-27 15:08:50 +0200

    2021/10/27 (1)

--- JavaAdvProjects/src/main/java/Lesson7/Collection/StudentSortName.java
+++ JavaAdvProjects/src/main/java/Lesson7/Collection/StudentSortName.java
@@ -3,6 +3,7 @@ package Lesson7.Collection;
 import java.util.Comparator;
 
 public class StudentSortName implements Comparator<Student>{
+    @Override
     public int compare(Student s1, Student s2){
         int result = s1.getName().compareTo(s2.getName());
         if (result != 0) { return result; }
